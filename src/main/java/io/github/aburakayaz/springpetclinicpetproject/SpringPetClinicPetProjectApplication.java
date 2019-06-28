06df6cb9974a66e2ed9959cf82a4cccc4b1ccb36
Burak Ayaz <adnan.burak.ayaz@gmail.com>
2019-06-28 15:55:33 +0300

    Spring Initialized

---
+++ src/main/java/io/github/aburakayaz/springpetclinicpetproject/SpringPetClinicPetProjectApplication.java
@@ -0,0 +1,13 @@
+package io.github.aburakayaz.springpetclinicpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicPetProjectApplication.class, args);
+    }
+
+}
